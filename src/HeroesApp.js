5633f3d43aa6e3b4809435f09d29b7ebbc1dd6bf
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-10-11 11:59:28 -0300

    Initialize project: remove unnecessary files in "/src" and install Bootstrap and Prettier

---
+++ src/HeroesApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const HeroesApp = (props) => {
+  return (
+    <div>
+      <h1>HeroesApp</h1>
+    </div>
+  )
+}
+
+HeroesApp.propTypes = {}
+
+export default HeroesApp
