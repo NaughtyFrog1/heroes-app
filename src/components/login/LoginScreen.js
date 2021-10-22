32570b1d3e72e12f7f7a6b62aa7b68953db06773
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-10-22 17:33:05 -0300

    LoginScreen is working with history replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-const LoginScreen = (props) => {
+const LoginScreen = ({ history }) => {  // 1.
+  const handleClickLogin = () => {
+    history.replace('/') // 2.
+  }
+
   return (
-    <div>
+    <div className="container mt-5">
       <h1>Login Screen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleClickLogin}>
+        Login
+      </button>
     </div>
   )
 }
 
-LoginScreen.propTypes = {}
-
 export default LoginScreen
+
+/**
+ * 1. El componente tiene varias props incrustadas por defecto por react-router,
+ * history es una de ellas. Podemos verlas desde la vista de componentes en 
+ * chrome
+ * 
+ * 2. Con `history.replace(url)' reemplazamos la entrada en el historial de la
+ * pestaña, haciendo que no se pueda volver para atras.
+ */
