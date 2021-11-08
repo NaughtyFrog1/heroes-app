c1f97ea06477d0760c20915135c037dfc7cfd373
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-11-08 19:23:15 -0300

    create authContext

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import AuthContext from './auth/AuthContext'
+import authReducer from './auth/authReducer'
 import AppRouter from './routers/AppRouter'
 
+const init = () => JSON.parse(localStorage.getItem('user')) || { logged: false }
+
 const HeroesApp = () => {
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+
   return (
-    <AppRouter />
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <AppRouter />
+    </AuthContext.Provider>
   )
 }
 
 export default HeroesApp
+
+/**
+ * <AuthContext.Provider value={{ user, dispatch }}>:
+ *    son los valores que vamos a distribuir con el AuthContext
+ */
---
+++ src/types/types.js
@@ -0,0 +1,11 @@
+const types = {
+  login: '[auth] login',
+  logout: '[auth] logout',
+}
+
+export default types
+
+/**
+ * Este objeto va a contener todos los types de la aplicación.
+ * Entre corchetes indicamos a que grupo de acciones pertenece el type
+ */
