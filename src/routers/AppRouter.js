59c3a25201c1e1fe56cfb17a0814aaab10a19519
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-11-09 22:34:43 -0300

    add PrivateRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,27 @@
 // Por convención se llama AppRouter para indicar que es el router principal de la aplicación
 
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import AuthContext from '../auth/AuthContext'
 import LoginScreen from '../components/login/LoginScreen'
 import DashboardRoutes from './DashboardRoutes'
+import PrivateRoute from './PrivateRoute'
 
 const AppRouter = () => {
+  const { user } = useContext(AuthContext)
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={DashboardRoutes} />
+
+          {/* 1. */}
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
@@ -19,3 +29,7 @@ const AppRouter = () => {
 }
 
 export default AppRouter
+
+/**
+ * 1. Como en ese router estan todas las rutas que queremos proteger, solo debemos proteger ese componente.
+ */
---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router'
+import PropTypes from 'prop-types'
+
+const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  )
+}
+
+PrivateRoute.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+}
+
+export default PrivateRoute
+
+/**
+ * Es un functional component igual a los usados hasta ahora.
+ */
